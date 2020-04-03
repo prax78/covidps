ab5d49b99cf2a934c98c7c8334eacca92b671598
prax78 <prax78@gmail.com>
2020-04-04 04:08:07 +0530

    Add files via upload

---
+++ getcountry.ps1
@@ -0,0 +1,39 @@
+﻿function Get-CounteryData()
+{
+  [cmdletbinding()]
+param
+(
+[Parameter(Mandatory)]
+$CountryName
+
+)
+
+$brdrcntry=@()
+
+$uri=Invoke-WebRequest -UseBasicParsing "https://restcountries.eu/rest/v2/name/$($CountryName)?fullText=true" -ContentType application/json
+$data=ConvertFrom-Json $uri
+
+foreach($border in $data.borders)
+{
+
+$brdrcntry+=(Invoke-WebRequest -UseBasicParsing "https://restcountries.eu/rest/v2/alpha/$($border)"|ConvertFrom-Json).name
+}
+
+$data=[pscustomobject]@{
+Country=$data.name
+Region=$data.region
+Capital=$data.capital  
+PopulationInBn=$data.population/1000000000 
+Lattiude=$data.latlng[0]
+Longitude=$data.latlng[1]
+Demonym=$data.demonym 
+AreaInSqKm=$data.area
+TimeZone=$data.timezones 
+BorderCountry=[string]::join(",",$brdrcntry)
+currency=$data.currencies.name
+Language=[string]::join(",",$data.languages.name)
+Flag=$data.flag
+Code=$data.cioc
+}
+$data
+}
