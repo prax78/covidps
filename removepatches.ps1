69124a83cb6012c201197058fe3c14ecab3f7c37
prax78 <prax78@gmail.com>
2021-12-08 17:44:56 +0530

    Add files via upload

---
+++ removepatches.ps1
@@ -0,0 +1,40 @@
+﻿function Remove-Patches()
+{
+  [cmdletbinding()]
+param
+(
+[Parameter(Mandatory)]
+$InstalledDate
+
+)
+
+$KB=@()
+
+$hotfixes=get-hotfix
+
+foreach($hotfix in $hotfixes)
+{
+
+if($hotfix.InstalledOn -contains $InstalledDate)
+{
+
+$KB+=$hotfix.HotFixID
+
+}
+
+}
+echo "Below KBs are found that were installed on $($InstalledDate)"
+$KB
+
+foreach($patch in $KB)
+{
+
+echo "Uninstalling $($patch)"
+$patchToUninstall=$patch -replace "KB",""
+Start-Process WUSA -ArgumentList "/uninstall /quiet /norestart /kb:$($patchToUninstall)" -Wait
+
+
+}
+echo "patches have been removed"
+}
+
